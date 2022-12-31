c849ed8cf320b066e3e142ceda37871dec6fdee2
simbolmina <simbolmina@hotmail.com>
2022-12-31 19:44:43 +0300

    eslint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
   },
   plugins: ["react"],
   rules: {
+    quotes: [2, "single", { avoidEscape: true }],
     "react/no-unescaped-entities": 0,
     "eslintreact/no-danger": 0,
     "react/jsx-max-props-per-line": 0,
